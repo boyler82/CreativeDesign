88e1f1dcc72866c4bf7113b6e48e0f1399779ea7
boyler82 <tomasz26bojko@gmail.com>
2024-04-23 22:22:47 +0200

    wersja gotowa do tesow

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Create observers for different sections
   createObserver('#about .phrase', { root: null, threshold: isMobile ? 0.5 : 1 }, 'active');
-  createObserver('#gallery .image-box', { root: null, threshold: isMobile ? 0.5 : 1 }, 'active');
+  // createObserver('#gallery .image-box', { root: null, threshold: isMobile ? 0.5 : 1 }, 'active');
   createObserver('#blog .featured-article, #blog .article', { root: null, threshold: isMobile ? 0.01 : 0.3 }, 'fadeInUp');
   createObserver('#contact > div', { root: null, threshold: isMobile ? 0.01 : 0.7 }, 'fadeInUp');
 });
@@ -76,3 +76,55 @@ document.getElementById('myForm').addEventListener('submit', function(event) {
       console.error('Error:', error);
   });
 });
+
+
+
+// dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+
+// Dark Mode
+const themeSwitcher = document.getElementById('theme-switcher');
+
+function updateThemeIcon(isDarkMode) {
+themeSwitcher.children[0].classList.replace(isDarkMode ? 'fa-sun' : 'fa-moon', isDarkMode ? 'fa-moon' : 'fa-sun');
+}
+
+// Determine if dark mode is preferred
+function prefersDarkMode() {
+return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+}
+
+// Set the theme based on the preference
+function setThemeBasedOnPreference() {
+const isDarkMode = prefersDarkMode();
+document.documentElement.setAttribute('data-theme', isDarkMode ? 'dark' : 'light');
+updateThemeIcon(isDarkMode);
+}
+
+// Switch Theme manually
+function switchTheme() {
+const currentTheme = document.documentElement.getAttribute('data-theme');
+const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+document.documentElement.setAttribute('data-theme', newTheme);
+localStorage.setItem('theme', newTheme);
+updateThemeIcon(newTheme === 'dark');
+}
+
+// Event Listener
+themeSwitcher.addEventListener('click', switchTheme);
+
+// Initialize Theme
+function initializeTheme() {
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) {
+    document.documentElement.setAttribute('data-theme', savedTheme);
+    updateThemeIcon(savedTheme === 'dark');
+} else {
+    setThemeBasedOnPreference();
+}
+}
+
+// Listen for system theme changes
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', setThemeBasedOnPreference);
+
+// Initialize theme when the script loads
+initializeTheme();
